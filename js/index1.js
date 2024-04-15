177b590297e4fdfd6b5e1f4a8b02f89581a902ea
CharlesZ921 <charles2001921@163.com>
2024-04-15 13:51:32 -0700

    block mobile access  (for now)

--- js/index1.js
+++ js/index1.js
@@ -10,4 +10,23 @@
       document.getElementById('about-me').innerHTML = 'Failed to load text.';
   });
 
+  /* block access from mobile*/
+  document.addEventListener("DOMContentLoaded", function() {
+    if (detectMobileDevice()) {
+        clearBodyAndShowMessage();
+    }
+  });
+
+  function detectMobileDevice() {
+      return window.innerWidth < 800 || navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i);
+  }
+
+  function clearBodyAndShowMessage() {
+      document.body.innerHTML = '';
+      const mobileBlock = document.createElement('div');
+      mobileBlock.setAttribute('id', 'mobile-block');
+      mobileBlock.innerHTML = '<p>Sorry! For now the website does not support mobile access, please visit through a computer.</p>';
+      document.body.appendChild(mobileBlock);
+  }
+
 })()
